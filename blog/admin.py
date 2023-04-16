1c5c248c9323d53248cb77122469589429674a58
damidaramola <damidaramola7@gmail.com>
2023-04-16 08:01:19 +0000

    add fields to admin.register post

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'blog']
     summernote_fields = ('blog')
+    list_filter = ('status', 'created_on')
