449e9186128e322682599329cb33a78569f7386c
damidaramola <damidaramola7@gmail.com>
2023-04-30 10:18:02 +0000

    add cloudinary_name

--- festivefun/settings.py
+++ festivefun/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 CLOUDINARY_STORAGE = {
-   
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
 }
