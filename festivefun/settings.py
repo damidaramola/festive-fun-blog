6fca4297282e693a6da35f01b008b27f472d8025
damidaramola <damidaramola7@gmail.com>
2023-04-30 10:42:00 +0000

    add cloudinary url before deployment

--- festivefun/settings.py
+++ festivefun/settings.py
@@ -156,7 +156,8 @@ USE_TZ = True
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
