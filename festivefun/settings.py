e9e7136861250d545adfffbe663124f95fa641d3
damidaramola <damidaramola7@gmail.com>
2023-04-30 10:44:53 +0000

    add deployment to heroku

--- festivefun/settings.py
+++ festivefun/settings.py
@@ -157,7 +157,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+    'CLOUDINARY_URL': os.environ.get("CLOUDINARY_URL"),
 }
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
