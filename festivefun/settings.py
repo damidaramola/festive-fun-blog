6626fd55e5eeff530592447ee10cbb415e37d5b8
damidaramola <damidaramola7@gmail.com>
2023-08-07 15:19:49 +0000

    remove cloudinary storage dictionary

--- festivefun/settings.py
+++ festivefun/settings.py
@@ -154,12 +154,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    'CLOUDINARY_URL': os.environ.get("CLOUDINARY_URL"),
-}
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
