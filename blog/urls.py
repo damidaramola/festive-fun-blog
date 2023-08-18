6da9b1a5919ebf2feb32a599c1bd3ccdedec31cc
damidaramola <damidaramola7@gmail.com>
2023-08-18 12:44:05 +0000

    add edit comment url path

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('about/', views.about, name='about_page'),
     path('<slug:post>/', views.SinglePost.as_view(), name='single_post'),
     path('clap/<slug:post>', views.ClapPosts.as_view(), name='clap_posts'),
+    path('edit_comment/<int:id>', views.edit_comment, name='edit_comment'),
     path('delete_comment/<int:id>', views.delete_comment,
          name='delete_comment'),
 
